a8cee5bf5e7c0165484e304b4e78c0cad163f421
Johann Blignaut <blignaut24@gmail.com>
2024-06-01 06:11:56 +0000

    fix(database): Corrected database configuration
    An error occurred when running the Django migration due to a misconfigured DATABASES setting in the [settings.py](http://settings.py/) file. The ENGINE value was missing, causing the migration to fail. The [settings.py](http://settings.py/) file has been updated with the correct ENGINE value.
    
    Resolves: Database configuration issue.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .tours/3-creating-the-database.tour
    	modified:   codestar/settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&m%gu-ez==#b@$+_t99xi_wgv()$4vr7#$-og^=x4rdhx6lh6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-blignaut24-djangoblog-957szijfqvs.ws-eu114.gitpod.io", 
                 '.herokuapp.com']
@@ -73,12 +77,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+DATABASE = {
+    'default': 
+dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -122,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
