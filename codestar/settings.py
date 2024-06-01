f94f0cee61b912f8531946cc200da7923f473c40
Johann Blignaut <blignaut24@gmail.com>
2024-06-01 09:08:48 +0000

    fix(database): Corrected DATABASES setting typo
    The error was due to a misnaming in the DATABASES setting in the Django configuration. The setting was erroneously named `DATABASE`. Compared the user's code with the source code from the walkthrough and identified the discrepancy. Suggested using tools like [diffchecker.com](http://diffchecker.com/) for spotting such differences in the future.
    
    Resolves: Database setting naming issue.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .tours/3-creating-the-database.tour
    	modified:   codestar/settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -84,11 +84,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
     }
 }
 '''
-DATABASE = {
-    'default': 
-dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
