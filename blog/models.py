0c2fc5693e251d869c337f2fda7cf220b780d792
Johann Blignaut <blignaut24@gmail.com>
2024-06-04 09:33:33 +0000

    docs: explain django migrations and their application I Think Therefore I Blob> Models Part 1> Inside Migrations

--- blog/models.py
+++ blog/models.py
@@ -15,5 +15,8 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
 
 
