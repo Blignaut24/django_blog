4b054dd91fce12753991d6b921ebecf3df48e418
Johann Blignaut <blignaut24@gmail.com>
2024-06-15 08:43:57 +0000

    Rich text & reload > Admin Panel Power-Up

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,8 +20,11 @@ from django.urls import path, include
 
 urlpatterns = [
     # path("blog/", blog_views.my_blog, name='blog'),
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
+    
+    
 ]
 
 
