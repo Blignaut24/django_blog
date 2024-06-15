4335a79599ac5a4a725bce933ba89eb400cba809
Johann Blignaut <blignaut24@gmail.com>
2024-06-15 11:59:31 +0000

    Template > The base template and homepage

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,8 @@ from .models import Post
    # template_name = "post_list.html"
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+   queryset = Post.objects.filter(status=1)
+   #template_name = "post_list.html"
+   template_name = "blog/index.html"
+   paginate_by = 6
 
