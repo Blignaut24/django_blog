05073d7cbdcb7523a35987aa4e68b9a0c835e828
Johann Blignaut <blignaut24@gmail.com>
2024-06-15 09:10:15 +0000

    Rich Text & Reload: Admin Panel Power Up

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&m%gu-ez==#b@$+_t99xi_wgv()$4vr7#$-og^=x4rdhx6lh6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["8000-blignaut24-djangoblog-krw5zen1ioo.ws-eu114.gitpod.io", 
