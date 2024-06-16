44e5ff111c86594c43437ad9ae99a216405363f6
Johann Blignaut <blignaut24@gmail.com>
2024-06-16 12:28:09 +0000

    fix: removed space in js file name to resolve console.log error

--- codestar/urls.py
+++ codestar/urls.py
@@ -23,8 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
-    
-    
 ]
 
 
