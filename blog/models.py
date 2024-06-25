daec451c8cc23a6f3773201ed58749b14cc84067
Johann Blignaut <blignaut24@gmail.com>
2024-06-25 20:41:57 +0000

    Lesson: I Think Therefore I Blog > Views Part 3 > POSTing and writing to the database

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,11 @@
-# Import necessary modules
 from django.db import models
 from django.contrib.auth.models import User
 
-# Define status options
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# ---------------------
-# Define Post Model
-# ---------------------
-# The 'Post' model is defined here with various fields.
-# The 'Meta' class is used to specify additional options.
-# The '__str__' method is overridden to return a user-friendly representation of the model.
+# Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -24,27 +19,20 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-            ordering = ["-created_on", "author"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-# ---------------------
-# Define Comment Model
-# ---------------------
-# The 'Comment' model is defined here with various fields.
-# The 'Meta' class is used to specify additional options.
-# The '__str__' method is overridden to return a user-friendly representation of the model.
+
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE,
-        related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name="commenter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
