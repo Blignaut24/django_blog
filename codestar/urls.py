1ed4369b7c1a4556c592c95f6eb9a70d3956a4ea
Johann Blignaut <blignaut24@gmail.com>
2024-06-26 03:59:46 +0000

    fix: Error page

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
-    path("accounts/", include("allauth.urls")),
 ]
