5f058fe1c62b8be140c0361d35cd5ad3c25fcc43
Johann Blignaut <blignaut24@gmail.com>
2024-06-26 04:35:30 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
